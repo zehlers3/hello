4b461fdb2e4fc5fd04814959816845d7e4724336
Zach <zehlers3@huskers.unl.edu>
2021-08-26 14:32:35 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * 
+ * Author: Zach Ehlers
+ * Date 2021/8/23
+ * 
+ * A sinmple hello world program
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv){
+    
+    printf("Zach Ehlers\n");
+    
+    return 0;
+    
+}
